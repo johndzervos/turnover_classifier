1575a10ffd21f42d0dbea37a6a73ed057f2488c6
johndzervos <ioannis@crunchrapps.com>
2022-02-20 22:48:34 +0100

    Treat categorical data

--- turnover_cls.py
+++ turnover_cls.py
@@ -9,56 +9,81 @@ ipython
 # Load libraries
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
-from sklearn.feature_extraction import DictVectorizer
-from sklearn.model_selection import train_test_split # Import train_test_split function
-from sklearn.preprocessing import LabelEncoder
-from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
 from six import StringIO  
 from pydotplus import graph_from_dot_data
 from IPython.display import Image 
 import random
+import numpy as np
 
-LABELS = ['a', 'b', 'c']
+LABELS = ['aa', 'bb', 'cc']
 COLUMNS = [
     'f1',
     'f2',
     'f3',
     'f4',
-    'Outcome'
+    'f5',
+    'f6',
+    'Outcome',
 ]
+CLASS_COLUMN = 'Outcome'
+CATEGORICAL_COLUMNS = ['f4', 'f5']
 
-CATEGORIES = ['one', 'two', 'three']
+CATEGORIES = ['zero', 'one', 'two', 'three']
+CATEGORIES_OR_NONE = ['four', 'five', 'six', 'seven', None]
+BOOL = [True, False]
 
 
 def generate_entries(cnt, low=1, high=10, label=None):
   """
-
+  Generate entries for the dataset.
   """
   return [
     [
-        random.randint(low, high),  # f1
-        random.randint(low, high),  # f2
-        random.randint(low, high),  # f3
-        random.choice(CATEGORIES),  # f4
+        random.randint(low, high),  # f1 Int
+        random_int_or_None(low, high),  # f2 Int or None
+        random.randint(low, high*100)/100,  # f3 Float
+        random.choice(CATEGORIES),  # f4 Categorical
+        random.choice(CATEGORIES_OR_NONE),  # f5 Categorical or None
+        random.choice(BOOL),  # f6 Boolean
         label
     ]
     for _ in range(cnt)
   ]
 
+def random_int_or_None(low, high):
+  """
+  Return a random int between low and high or None
+  """
+  number = random.randint(-1, 5)
+  if number < 0:
+    return None
+  else:
+    return random.randint(low, high)
+
+
 def generate_dataset():
   """
-
+  Generate dataset with specific classes
   """
-  feature_cols = COLUMNS[:-1]
-  class_col = COLUMNS[-1]
-  df1 = pd.DataFrame(generate_entries(500, 1, 10, LABELS[0]), columns=COLUMNS)
-  df2 = pd.DataFrame(generate_entries(100, 100, 200, LABELS[1]), columns=COLUMNS)
-  df3 = pd.DataFrame(generate_entries(50, 1, 200, LABELS[2]), columns=COLUMNS)
+  df1 = pd.DataFrame(generate_entries(100, 1, 10, LABELS[0]), columns=COLUMNS)
+  df2 = pd.DataFrame(generate_entries(100, 5, 10, LABELS[1]), columns=COLUMNS)
+  df3 = pd.DataFrame(generate_entries(100, 1, 10, LABELS[2]), columns=COLUMNS)
   
-  result = pd.concat([df1, df2, df3])
-  print(result)
-  print(len(result))
-  return result[feature_cols], result[class_col], feature_cols
+  return pd.concat([df1, df2, df3]).fillna(0)
+
+def get_feature_data_class_data_and_columns(data):
+  """
+  Return the feature data, the class column and the list of the feature columns
+  In case of categorical data, a new column is created for each of the provided category
+  """
+  print(data)
+  # Cope with categorical data
+  data = pd.get_dummies(data, columns=CATEGORICAL_COLUMNS)
+  print(data)
+  feature_data = data.loc[:, data.columns!=CLASS_COLUMN]
+  return feature_data, data[CLASS_COLUMN], list(feature_data.columns)
 
 def calculate_feature_importance(clf, feature_cols):
   # feat_importance = clf.tree_.compute_feature_importances(normalize=False)
@@ -83,29 +108,11 @@ def visualize_classifier(clf, feature_cols):
   graph.write_png('decision_tree_classifier.png')
   Image(graph.create_png())
 
-X, y, feature_cols = generate_dataset()
-
-# Cope with categorical data
-print(X)
-
-X_feature = X[feature_cols]
-X_dict = X_feature.T.to_dict().values()
-
-# turn list of dicts into a numpy array
-vect = DictVectorizer(sparse=False)
-X_vector = vect.fit_transform(X_dict)
-
-# Used to vectorize the class label
-le = LabelEncoder()
-y = le.fit_transform(y)
-print(X)
-
+data = generate_dataset()
+X, y, feature_cols = get_feature_data_class_data_and_columns(data)
 
 # Split dataset into training set and test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1) # 70% training and 30% test
-
-# print(X_train)
-# print(X_test)
 
 # Create Decision Tree classifer object
 clf = DecisionTreeClassifier()
@@ -116,9 +123,18 @@ clf = clf.fit(X_train, y_train)
 # Predict the response for test dataset
 y_pred = clf.predict(X_test)
 
-random_entries = generate_entries(5, 1, 200)
-random_pred = clf.predict(X_test[feature_cols])
-print(random_entries)
+random_entries = pd.DataFrame(generate_entries(5, 1, 10), columns=COLUMNS).fillna(0)
+random_data, _, _, = get_feature_data_class_data_and_columns(random_entries)
+
+# Fill empty columns with zeros
+for col in feature_cols:
+  if col not in random_data.columns:
+    random_data[col] = 0
+# Reorder columns and remove columns that do not exist in the feature_cols
+random_data = random_data[feature_cols]
+
+print(random_data)
+random_pred = clf.predict(random_data)
 print(random_pred)
 
 # Calculating feature importance
